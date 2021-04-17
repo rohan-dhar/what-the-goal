f74950cfbde65168b833ee44d5cb5d7cb27b1d44
Rohan Dhar <rohan19443@iiitd.ac.in>
2021-04-17 08:02:14 +0530

    Add navigation

--- server/auth.py
+++ server/auth.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, session
 from flask.globals import request
+from flask.views import View
 from flask.helpers import url_for
 from werkzeug.utils import redirect
-from .generic_views import LoggedOutView
+from .generic_views import LOGGED_OUT_PATH, LoggedOutView, LoggedInView
 
 
 class LoginView(LoggedOutView):
@@ -25,5 +26,12 @@ class LoginView(LoggedOutView):
         return super().render(error=error)
 
 
+class LogoutView(View):
+    def dispatch_request(self):
+        session.pop("user_id")
+        return redirect(url_for(LOGGED_OUT_PATH))
+
+
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 auth_bp.add_url_rule("/login", view_func=LoginView.as_view(name="login"))
+auth_bp.add_url_rule("/logout", view_func=LogoutView.as_view(name="logout"))
